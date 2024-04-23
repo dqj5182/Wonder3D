1e8408c5d9a0ab2bb94ee217bdfd5dde30704bd6
dqj5182 <dqj5182@snu.ac.kr>
2024-04-23 20:27:54 +0000

    organize code

--- instant-nsr-pl/launch.py
+++ instant-nsr-pl/launch.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import logging
 from datetime import datetime
 import datasets
 import systems
@@ -14,20 +13,16 @@ from utils.misc import load_config
 
 
 def main(exp_dir, runs_dir):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default='configs/neuralangelo-ortho-wmask.yaml', help='path to config file')
-    parser.add_argument('--gpu', default='0', help='GPU(s) to be used')
-    args, extras = parser.parse_known_args()
+    config = load_config('configs/neuralangelo-ortho-wmask.yaml', cli_args=['--train', 'dataset.root_dir=../outputs/cropsize-192-cfg1.0/', 'dataset.scene=owl'])
+    config.cmd_args = {'config': 'configs/neuralangelo-ortho-wmask.yaml', 'gpu': '0'}
 
-    config = load_config(args.config, cli_args=extras)
-    config.cmd_args = vars(args)
-
-    config.trial_name = config.get('trial_name') or (config.tag + datetime.now().strftime('@%Y%m%d-%H%M%S'))
+    trial_name = datetime.now().strftime('@%Y%m%d-%H%M%S')
     exp_dir = os.path.join(exp_dir, config.name)
-    config.save_dir = config.get('save_dir') or os.path.join(exp_dir, config.trial_name, 'save')
-    config.ckpt_dir = config.get('ckpt_dir') or os.path.join(exp_dir, config.trial_name, 'ckpt')
-    config.code_dir = config.get('code_dir') or os.path.join(exp_dir, config.trial_name, 'code')
-    config.config_dir = config.get('config_dir') or os.path.join(exp_dir, config.trial_name, 'config')
+    config.save_dir = os.path.join(exp_dir, trial_name, 'save')
+    config.ckpt_dir = os.path.join(exp_dir, trial_name, 'ckpt')
+    config.code_dir = os.path.join(exp_dir, trial_name, 'code')
+    config.config_dir = os.path.join(exp_dir, trial_name, 'config')
+    # import pdb; pdb.set_trace()
 
     num_seed = 42
     pl.seed_everything(num_seed)
@@ -54,8 +49,8 @@ def main(exp_dir, runs_dir):
     # Logger
     loggers = []
     loggers += [
-        TensorBoardLogger(runs_dir, name=config.name, version=config.trial_name),
-        CSVLogger(exp_dir, name=config.trial_name, version='csv_logs')
+        TensorBoardLogger(runs_dir, name=config.name, version=trial_name),
+        CSVLogger(exp_dir, name=trial_name, version='csv_logs')
     ]
     # import pdb; pdb.set_trace()
     # Fitting    
